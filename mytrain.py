58cfacc758087aba921401fbf202f5f96e1b2823
silvey <ly1164@nyu.edu>
2022-09-22 15:22:37 +0400

    +

--- mytrain.py
+++ mytrain.py
@@ -20,7 +20,7 @@ trainer = Trainer(
     '../cifar10png/png1/',
     train_batch_size = 32,
     train_lr = 8e-5,
-    train_num_steps = 100,         # total training steps
+    train_num_steps = 2,         # [should edit] total training steps
     gradient_accumulate_every = 2,    # gradient accumulation steps
     ema_decay = 0.995,                # exponential moving average decay
     amp = False                        # turn on mixed precision
